aa1541fa2804860fd632c79576d82f70fd8bb5e3
daniel <danzack763@gmail.com>
2022-09-24 17:14:01 +0800

    addition

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 print("Welcome to simple calculator")
 
-get_input = input("Enter a number: ")
-print("Your input: " + get_input)
+get_input_1 = int(input("Enter a number: "))
+
+get_input_2 = int(input("Enter 2nd number: "))
+
+print("Added together: ",  (get_input_1 + get_input_2))
