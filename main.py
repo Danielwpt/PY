0573d40f90318df6b230bc611ed71ce512b65e95
daniel <danzack763@gmail.com>
2022-09-25 00:55:45 +0800

    using 1st class function

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-print("Welcome to simple calculator")
+print("Welcome to simple calculator\n")
 
 get_input_1 = int(input("Enter a number: "))
 
 get_input_2 = int(input("Enter 2nd number: "))
 
-print("Added together: ",  (get_input_1 + get_input_2))
+def addition(x, y):
+    return x + y
+
+arr_methods = [addition]
+
+print("Added together: ",  arr_methods[0](get_input_1, get_input_2))
