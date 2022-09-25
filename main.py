21e047e786e1b956221058db09fb4ac8c24eef77
daniel <danzack763@gmail.com>
2022-09-25 21:05:01 +0800

    Add operand choice

--- main.py
+++ main.py
@@ -7,6 +7,14 @@ get_input_2 = int(input("Enter 2nd number: "))
 def addition(x, y):
     return x + y
 
-arr_methods = [addition]
+def subtract(x, y):
+    return x - y
 
-print("Added together: ",  arr_methods[0](get_input_1, get_input_2))
+arr_methods = [addition, subtract]
+
+choose_operand = int(input("1: Add, 2: Sub = "))
+
+if choose_operand == arr_methods[0]:
+    print("Added together: " , addition(get_input_1, get_input_2))
+else:
+    print("Subs together: ", subtract(get_input_1, get_input_2))
