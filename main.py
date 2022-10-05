fb320564895c816cae8755a56fd746d7760f383e
daniel <danzack763@gmail.com>
2022-10-05 21:51:25 +0800

    add divide function

--- main.py
+++ main.py
@@ -13,13 +13,18 @@ def subtract(x, y):
 def multiply(x, y):
     return x * y
 
-arr_methods = [addition, subtract, multiply]
+def divide(x, y):
+    return x / y
 
-choose_operand = int(input("0: Add, 1: Sub, 2: Multiply = "))
+arr_methods = [addition, subtract, multiply, divide]
+
+choose_operand = int(input("0: Add, 1: Sub, 2: Multiply, 3: Divide = "))
 
 if arr_methods[choose_operand] == arr_methods[0]:
     print("Added together: " , addition(get_input_1, get_input_2))
 elif arr_methods[choose_operand] == arr_methods[1]:
     print("Subs together: ", subtract(get_input_1, get_input_2))
+elif arr_methods[choose_operand] == arr_methods[2]:
+    print("Multiply together: ", multiply(get_input_1, get_input_2))
 else:
-    print("Subs together: ", multiply(get_input_1, get_input_2))
+    print("Divide together: ", divide(get_input_1, get_input_2))
